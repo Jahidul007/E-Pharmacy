9004a43584d0ba6108a62f0ae89e903bfb9db2cc
Jahidul Islam <jahidul479@gmail.com>
2018-10-14 10:21:26 +0600

    Update Constants.java

--- app/src/main/java/com/tutorial/authentication/utils/Constants.java
+++ app/src/main/java/com/tutorial/authentication/utils/Constants.java
@@ -2,8 +2,8 @@ package com.tutorial.authentication.utils;
 
 public class Constants {
 
-    public static final String FIREBASE_URL = "https://android-firebase-authent-262ea.firebaseio.com/";
-    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_URL = "your firebase url";
+    public static final String FIREBASE_LOCATION_USERS = "your location";
     public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
     public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
 }
